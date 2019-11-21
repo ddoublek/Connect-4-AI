0703bee5f7df22a396add0d0afbe9c4f649ac167
David Kim <dwk421@gmail.com>
2019-11-21 17:34:10 -0500

    Changed to be able to play more generally (not assignment)

--- main.cpp
+++ main.cpp
@@ -7,13 +7,30 @@ int main(){
 
 	Board board;
 
-	std::cout << "p" << std::endl;
+	
 	
 
 	// Read in an integer so you know will go first.  This will be a 1 if your program goes first, a 2 if your program goes second
 	int move_first;
 	int best;
-	std::cin >> move_first;
+
+	while(true){
+		std::cout << "Type 1 for AI first, 2 for player first" << std::endl;
+		std::cin >> move_first;
+		if(move_first == 1 || move_first == 2){
+			break;
+		}
+		else{
+			std::cout << "Invalid move: " << move_first << std::endl;
+		}
+	}
+
+	
+
+
+
+
+
 	board.print();
 	if ( move_first == 1 )
 	{
@@ -22,27 +39,41 @@ int main(){
 		best = board.getBest();
 		board.makeMove(best+1);
 		board.print();
-		std::cout << best+1; 
 
 	}
 	else if ( move_first == 2 )
 	{
 		board.isSecond();
-		// if you go second, print out a ? so the moderator knows your program is ready
-		std::cout << "?";
 	}
 
 	int opp_move;
 	while ( std::cin >> opp_move )
 	{
-		if ( opp_move <= 0 || opp_move > 7)
-			break;
 
-		board.makeMove(opp_move);
+		if(opp_move == -1){
+			break;
+		}
+
+		if ( opp_move <= 0 || opp_move > 7){
+			std::cout << "Invalid move: " << opp_move << std::endl;
+			continue;
+		}
+
+		bool valMove = board.makeMove(opp_move);
+
+		if(!valMove){
+			std::cout << "Invalid move: " << opp_move << std::endl;
+			continue;
+		}
+
 		best = board.getBest();
 		board.makeMove(best+1);
 		board.print();
-		std::cout << best+1; 
+		// std::cout << best+1; 
+		if(board.gameOver()){
+			std::cout << "Game is over" << std::endl;
+			break;
+		}
 	}
 
 	return 0;
